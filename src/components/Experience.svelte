913100038479f3f62e52a73829f7b1906f4112d3
Danny Joseph <DjosephNP@gmail.com>
2024-03-04 00:14:06 -0500

    Refactored sections

---
+++ src/components/Experience.svelte
@@ -0,0 +1,58 @@
+<section class="experience" id="experience">
+    <h2 class="heading">Experience</h2>
+
+    <div class="timeline-items">
+        <div class="timeline-item">
+            <div class="timeline-dot"></div>
+                <div class="timeline-date">
+                    Aug 2022 - Present
+                </div>
+                <div class="timeline-content">
+                    <h3>
+                        <span>Software Engineer</span>
+                    </h3>
+                    <h3>
+                        <span>Truth Technologies Inc.</span>
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates error animi nam ducimus magni iure quo facilis quaerat iste amet, cumque eos voluptatem in hic dicta ad neque dolores! Inventore.</p>
+                </div>
+            
+        </div>
+
+        <div class="timeline-item">
+            <div class="timeline-dot"></div>
+                <div class="timeline-date">
+                    May - Aug 2023
+                </div>
+                <div class="timeline-content">
+                    <h3>
+                        <span>Backend Engineer Intern</span>
+                    </h3>
+                    <h3>
+                        <span>Honeywell International</span>
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates error animi nam ducimus magni iure quo facilis quaerat iste amet, cumque eos voluptatem in hic dicta ad neque dolores! Inventore.</p>
+                </div>
+            
+        </div>
+
+        <div class="timeline-item">
+            <div class="timeline-dot"></div>
+                <div class="timeline-date">
+                    May - Aug 2022
+                </div>
+                <div class="timeline-content">
+                    <h3>
+                        <span>Software Developer</span>
+                    </h3>
+                    <h3>
+                        <span>IBM</span>
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates error animi nam ducimus magni iure quo facilis quaerat iste amet, cumque eos voluptatem in hic dicta ad neque dolores! Inventore.</p>
+                </div>
+            
+        </div>
+
+    </div>
+
+</section>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,33 @@
+<section class="skills" id="skills">
+    <h2 class="heading">Skills</h2>
+
+    <div class="skills-container">
+        <div class="skill-box">
+          <div class="skill-info">
+            <h4>Database Management</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quidem unde quasi aperiam porro dicta nam velit cupiditate expedita delectus repellendus, animi culpa dolorum ratione mollitia reiciendis voluptates dolores quam.</p>
+          </div>
+        </div>
+
+        <div class="skill-box">
+          <div class="skill-info">
+            <h4>API Architecture</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quidem unde quasi aperiam porro dicta nam velit cupiditate expedita delectus repellendus, animi culpa dolorum ratione mollitia reiciendis voluptates dolores quam.</p>
+          </div>
+        </div>
+
+        <div class="skill-box">
+          <div class="skill-info">
+            <h4>Web Development</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quidem unde quasi aperiam porro dicta nam velit cupiditate expedita delectus repellendus, animi culpa dolorum ratione mollitia reiciendis voluptates dolores quam.</p>
+          </div>
+        </div>
+
+        <div class="skill-box">
+          <div class="skill-info">
+            <h4>Unit/E2E Testing</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quidem unde quasi aperiam porro dicta nam velit cupiditate expedita delectus repellendus, animi culpa dolorum ratione mollitia reiciendis voluptates dolores quam.</p>
+          </div>
+        </div>
+    </div>
+</section>
